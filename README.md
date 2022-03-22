bec45dbfe81c20aa7becc993b87ae22fbcb3e6fa
Bank-Builder <andrew.turpin@cyber-mint.com>
2022-03-22 17:21:15 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,14 +1,41 @@
 # nmaq
 
-`nmaq` or quick nmap is a tool to quickly run detailed scans against a list of ip's or hostnames specified in the `nmaq.list` file
+`nmaq` a play on `nmap` but with q for **quaint** instead is a tool to bulk run detailed nmap scans against a list of ip's or hostnames specified in the `nmaq.list` file.
 
-Usage:
+> **WARNING** : Be CAREFUL and HAVE PERMISSION to run scans against production hosts.  It it quite possible that one or more of such hosts could be running [fail2ban]() or an equivalent and you will effectively lock yourself out of all the hosts with one press of the ENTER key!
+
+## Requirements
 ```
-nmaq nmaq.file`
+sudo apt install xsltproc
+# It is a good idea to check the github repo for a more up to date version
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+sudo apt install ./wkhtmltox_0.12.6-1.bionic_amd64.deb --install-recommends
 ```
 
+## Installation
+`nmaq` is a simple shell script with an accompanying .list file.  It may be downloaded and installed b cloning this repository.
 
 
+
+Usage
+```
+Usage nmaq [OPTION]... [FILE]
+   Scans the list of ip's or url's provided in a file using nmap
+   and produces .html and .pdf reports.
+
+  OPTIONS:
+    -f, --file      supply file name with ip's or urls to test
+
+    -v, --verbose   display vebose details and progress bar.
+    -q, --quiet     produces no terminal output
+        --help      display this help and exit
+        --version   display version and exit
+
+  Example:
+      nmaq -f myip.list
+```
+
+> `namq.list` is just a file of IP's, one per line with a space on the end of each line.
 
 ---
-(c) Copyright 2022, Bank-Builder.  Licensed under  open source MIT license.
+(c) Copyright 2019-2022, Bank-Builder. Licensed under open source MIT license.
--- nmaq.sh
+++ nmaq.sh
@@ -1,15 +1,100 @@
 #!/bin/bash
+# Copyright (c) 2018-2022, Andrew Turpin
+# License MIT: https://opensource.org/licenses/MIT
 
-if [ ! -f $1 ] 
-then
-    echo "Error: Must supply file"
-    exit
-fi 
+_version="0.2"
 
-while read -r line 
-do
-    host=`echo $line | cut -d" " -f1`
-    echo "Scanning $host : $port " 
-    sudo nmap -A --osscan-guess -p "*" -sS -v -oX $host.xml  --open --reason $host
-    xsltproc $host.xml > $host.html
-done < $1
+function displayHelp(){
+ echo "Usage nmaq [OPTION]... [FILE]";
+ echo "   Scans the list of ip's or url's provided in a file using nmap";
+ echo "   and produces .html and .pdf reports.";
+ echo "";
+ echo "  OPTIONS:";
+ echo "    -f, --file      supply file name with ip's or urls to test";
+ echo "";
+ echo "    -v, --verbose   display vebose details and progress bar.";
+ echo "    -q, --quiet     produces no terminal output";
+ echo "        --help      display this help and exit";
+ echo "        --version   display version and exit";
+ echo "";
+ echo "  Example:";
+ echo "      nmaq -f myip.list";
+ echo "";
+}
+
+function displayVersion(){
+ echo "nmaq (bank-builder utils) version $_version";
+ echo "Copyright (C) 2022, Bank Builder";
+ echo "License MIT: https://opensource.org/licenses/MIT";
+ echo "";
+}
+
+function ProgressBar {
+# $1 = progress_number, $2 = total
+# Author: Teddy Skarin, licensed under The Unlicense, https://github.com/fearside/ProgressBar/
+
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+    # Build progressbar string lengths
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+    printf "\rProgress : [${_fill// /\#}${_empty// /-}] ${_progress}%%"
+}
+
+function qTestFile(){
+    IPLIST="$1"
+
+    IFS=$'\n'
+    total=`cat $IPLIST | wc -l`
+    for iprow in $(cat $IPLIST)
+    do
+        host=$( echo "$iprow" |cut -d' ' -f1 );
+        echo "Scanning $host" 
+        sudo nmap -Pn -A --osscan-guess -p "*" -sS -oX $host.xml --stylesheet nmap.xsl --noninteractive --open --reason $host
+    done
+    for iprow in $(cat $IPLIST)
+    do
+        host=$( echo "$iprow" |cut -d' ' -f1 );
+        echo "Create report for $host" 
+        xsltproc $host.xml > $host.html
+        #wkhtmltopdf $host.html $host.pdf
+    done
+}
+
+
+# nmaq Main
+_verbose="0"
+_quiet="0"
+while [[ "$#" > 0 ]]; do
+    case $1 in
+        --help) 
+            displayHelp; exit 0;;
+        --version) 
+            displayVersion; exit 0;;
+        -f|--file) 
+            _nmaqfile="$2";
+            shift;;
+        -v|--verbose) 
+            _verbose="1";
+            ;;
+        -q|--quiet) 
+            _quiet="1";
+            ;;            
+        *) echo "Unknown parameter passed: $1"; exit 1;;
+    esac; 
+    shift; 
+done
+
+if [[ "$_quiet" == "0" ]]; then 
+    _title="nmaq ver $_version";
+fi;
+
+if [[ -n "$_nmaqfile" ]]; then 
+    qTestFile ${_nmaqfile};
+fi;
+
+
+echo "Try nmaq --help for help";
+
+## End ##
